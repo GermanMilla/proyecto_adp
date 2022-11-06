59ede8a53bbffdd81c80d690b3196ff656f29ad0
GermanMilla <pymosv@gmail.com>
2022-11-05 19:26:06 -0600

    nuevo modelo para la tablas de entradas y salidas, asi como nueva migracion

---
+++ database/migrations/2022_11_05_232217_create_entradas_salidas_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntradasSalidasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entradas_salidas', function (Blueprint $table) {
+            $table->id();
+            $table->string('Codigo');
+            $table->integer('Edicion');
+            $table->date('Fecha_validez');
+            $table->longText('reservado_gerencia');
+            $table->date('fecha');
+            $table->string('Ubicacion_equipo');
+            $table->string('tipo_movimiento');
+            $table->string('descripcion_equipo');
+            $table->string('marca');
+            $table->string('modelo');
+            $table->string('serie');
+            $table->boolean('Nuevo');
+            $table->boolean('usado_buen_estado');
+            $table->boolean('danado');
+            $table->longText('Diagnostico_u_observaciones');
+            $table->date('movimiento_desde');
+            $table->date('movimiento_hasta');
+            $table->boolean('movimiento_indefinido');
+            $table->string('persona_que_entrega');
+            $table->boolean('entregado_firma');
+            $table->string('persona_que_recibe');
+            $table->boolean('recibido_firma');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entradas_salidas');
+    }
+}
